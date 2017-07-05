9c8a659344a7e7d225a350323d7dd68a1ba06091
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-07-04 22:55:35 -0300

    Cria serviço de restaurantes

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpModule } from '@angular/http'
+import { RouterModule } from '@angular/router'
 
 import { ROUTES } from './app.routes'
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
+import { RestaurantsComponent } from './restaurants/restaurants.component'
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantsService } from './restaurants/restaurants.service'
 
 @NgModule({
   declarations: [
@@ -27,7 +27,7 @@ import { RestaurantComponent } from './restaurants/restaurant/restaurant.compone
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
