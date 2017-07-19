536032385361b176447a5da3c3cad11600a0e8fd
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-07-18 23:12:07 -0300

    Ajusta menu

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import { Restaurant } from './restaurant/restaurant.model'
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model'
 import { MEAT_API } from '../app.api'
 import { ErrorHandler } from '../app.error-handler'
 
@@ -26,6 +27,12 @@ export class RestaurantsService {
 
   reviewsOfRestaurants(id: string): Observable<any> {
     return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
+  menuOfRestaurants(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
