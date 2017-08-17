45ff0db18ca1509761f1cca25014a2040bce4d93
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-08-16 22:56:51 -0300

    Adiciona componente snackbar

--- meat-app-starter/src/app/shared/shared.module.ts
+++ meat-app-starter/src/app/shared/shared.module.ts
@@ -5,21 +5,23 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { InputComponent } from './input/input.component'
 import { RadioComponent } from './radio/radio.component'
 import { RatingComponent } from './rating/rating.component'
+import { SnackbarComponent } from './messages/snackbar/snackbar.component'
 
 import { OrderService } from '../order/order.service'
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
 import { RestaurantsService } from '../restaurants/restaurants.service'
+import { NotificationService } from './messages/notification.service'
 
 @NgModule({
-  declarations: [InputComponent, RadioComponent, RatingComponent],
+  declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [InputComponent, RadioComponent, RatingComponent, CommonModule, FormsModule, ReactiveFormsModule]
+  exports: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [OrderService, ShoppingCartService, RestaurantsService]
+      providers: [OrderService, ShoppingCartService, RestaurantsService, NotificationService]
     }
   }
 }
