acd0ad41a11552f31b5d4b3f035f397255d6f412
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-09-07 16:51:28 -0300

    Adiciona estrategia de navegação hash

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule, LOCALE_ID } from '@angular/core'
 import { HttpModule } from '@angular/http'
 import { RouterModule, PreloadAllModules } from '@angular/router'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { LocationStrategy, HashLocationStrategy } from '@angular/common'
 
 import { ROUTES } from './app.routes'
 
@@ -43,7 +44,10 @@ import { NotFoundComponent } from './not-found/not-found.component'
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
