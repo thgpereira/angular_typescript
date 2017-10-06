656ec569f6199cab88fb8dff4fe6a2c48631d90b
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-10-06 00:03:18 -0300

    Atualiza módulo http

--- meat-app-starter/src/app/order/order.service.ts
+++ meat-app-starter/src/app/order/order.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { Http, Headers, RequestOptions } from '@angular/http'
+import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
 import { MEAT_API } from '../app.api'
 import 'rxjs/add/operator/map'
@@ -10,7 +10,7 @@ import { Order, OrderItem } from './order.model'
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService, private http: Http) { }
+  constructor(private cartService: ShoppingCartService, private http: HttpClient) { }
 
   itemsValue(): number {
     return this.cartService.total()
@@ -37,10 +37,7 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    const headers = new Headers()
-    headers.append('Content-Type', 'application/json')
-    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({ headers: headers }))
-      .map(response => response.json())
+    return this.http.post<Order>(`${MEAT_API}/orders`, order)
       .map(order => order.id)
   }
 
