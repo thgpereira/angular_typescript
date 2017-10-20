3e26ace93b76b4bfaf067ada82da0df462b4cef8
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-10-20 15:06:08 -0200

    Adiciona segurança na compra

--- meat-app-starter/src/app/order/order.service.ts
+++ meat-app-starter/src/app/order/order.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
 import { MEAT_API } from '../app.api'
 import 'rxjs/add/operator/map'
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model'
 import { Order, OrderItem } from './order.model'
+import { LoginService } from '../security/login/login.service'
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService, private http: HttpClient) { }
+  constructor(private cartService: ShoppingCartService, private http: HttpClient, private loginService: LoginService) { }
 
   itemsValue(): number {
     return this.cartService.total()
@@ -37,7 +38,13 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    return this.http.post<Order>(`${MEAT_API}/orders`, order)
+    let headers = new HttpHeaders()
+    console.log("Logado: " + this.loginService.isLoggedIn())
+    console.log("Token: " + this.loginService.user.accessToken)
+    if (this.loginService.isLoggedIn()) {
+      headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
+    }
+    return this.http.post<Order>(`${MEAT_API}/orders`, order, { headers: headers })
       .map(order => order.id)
   }
 
